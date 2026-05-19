333c6231b9e6387aa942d5ad750fdac30a6fa182
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-05-19 14:39:48 +0530

    feat: initialize Cab Invoice Generator project with fare calculation logic and unit tests

---
+++ src/test/java/com/bridgelabz/invoicegenerator/CabInvoiceGeneratorTest.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.invoicegenerator;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CabInvoiceGeneratorTest {
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        assertEquals(25.0, fare, 0.0);
+    }
+
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
+        CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        assertEquals(5.0, fare, 0.0);
+    }
+}
